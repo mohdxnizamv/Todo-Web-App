54c165378cd32e8dc2aeb46464cf6533453fba7c
mohdxnizamv <nizam.active@gmail.com>
2022-08-17 15:54:35 +0800

    create todo web app

--- src/App.js
+++ src/App.js
@@ -1,41 +1,11 @@
-import { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Container} from "react-bootstrap"
 
 function App() {
-	const [myName, setMyName] = useState('mohdxnizamv');
-	const [myAge, setMyAge] = useState(25);
-
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-
-				<p>{myName}</p>
-
-				<button onClick={() => setMyName('Dea Afrizal')} className="mt-5">
-					klik untuk tukar nama
-				</button>
-
-				<div className="d-flex mt-3">
-					<button
-						onClick={() => setMyAge((state) => state + 1)}
-						className="me-5"
-					>
-						+
-					</button>
-
-					<p>{myAge}</p>
-
-					<button
-						onClick={() => setMyAge((state) => state - 1)}
-						className="ms-5"
-					>
-						-
-					</button>
-				</div>
-			</header>
-		</div>
+		<Container>
+			<h1 className='text-center mt-5'>Todo Web App</h1>
+		</Container>
 	);
 }
 
