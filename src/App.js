0790938eb79b443fb4b5ed47bcc29dcd2f1603bd
mohdxnizamv <nizam.active@gmail.com>
2022-08-17 16:49:53 +0800

    create todo web app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
+import { useState} from 'react';
 import './App.css';
-import {Container} from "react-bootstrap"
+import { Container, Form, Button, Stack, Card, Row, Col } from "react-bootstrap"
 
 function App() {
+  const [todoName, setTodoName] = useState("");
+
+  const [todoList, setTodoList] = useState([]);
+
+  const addTodo = () => {};
+
+  const handleChangeTodoName = (e) => {const data = e.target.value;console.log(data);};
+
 	return (
 		<Container>
 			<h1 className='text-center mt-5'>Todo Web App</h1>
+
+			{/* Input Todo */}
+			<Card className='mt-5'>
+				<Card.Body>
+					<Stack direction='horizontal' gap={3}>
+						<Form.Control type="text" placeholder="Masukan todo.." onChange={handleChangeTodoName}/>
+						<Button>Tambah </Button>
+					</Stack>
+				</Card.Body>
+			</Card>
+
+			{/* List Todo */}
+			<Card className='mt-5'>
+				<Card.Body>
+					<Row>
+            {/* Nama Todo */}
+						<Col>
+							<h3>Nama Todo</h3>
+						</Col>
+
+						{/* Button Todo*/}
+						<Col xl="1">
+							<Button>Selesai</Button>
+						</Col>
+					</Row>
+				</Card.Body>
+			</Card>
 		</Container>
 	);
 }
